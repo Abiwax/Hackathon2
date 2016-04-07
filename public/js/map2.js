a4ab0ede8fd874e18c6f701115ca330761049e91
Abiwax <abiwaxade@gmail.com>
2016-04-07 20:44:57 -0300

    record

--- public/js/map2.js
+++ public/js/map2.js
@@ -64,9 +64,6 @@ var unit = listbox4.options[selIndex4].value;
 console.log(record.map(function(rec){
           return rec."Annual Rent";
         }))
- console.log(record.map(function(rec){
-          return rec[0];
-        }))
 var data2 = {
     labels: record2.map(function(rec){
           return rec.Year;
